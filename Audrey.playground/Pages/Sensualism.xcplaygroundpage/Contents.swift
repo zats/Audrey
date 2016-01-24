2c3d6dce1f70818eec14b383774aee13dbedac8f
Sash Zats <zats@Tamars-MacBook-Air.local>
2016-01-24 13:35:28 -0800

    New Sensualism

--- Audrey.playground/Pages/Sensualism.xcplaygroundpage/Contents.swift
+++ Audrey.playground/Pages/Sensualism.xcplaygroundpage/Contents.swift
@@ -1,46 +1,97 @@
-//: Playground - noun: a place where people can play
-
 import Foundation
-import Sensualism
+//MARK: - Tokens
 
-
-// Query
-let query = "Get uber to 430 Hayes Street"
-
-// Tagger
-let schemes = NSLinguisticTagger.availableTagSchemesForLanguage("en")
-let options: NSLinguisticTaggerOptions = [.OmitWhitespace, .OmitPunctuation, .JoinNames]
-let tagger = NSLinguisticTagger(tagSchemes: schemes, options: Int(options.rawValue))
-
-// Plugins
-let plugins: [TokenizerPlugin] = [
-    LemmaPlugin(tagger: tagger, options: options),
-    LexicalClassPlugin(tagger: tagger, options: options),
-    AddressDetectingPlugin()
-]
-
-// Extract tokens
-let tokenizer = Tokenizer(plugins: plugins)
-let tokens = tokenizer.tokenize(query, fallbackPrefix: "Could you please ")
-
-
-enum Token {
-    case Lemma(original)
-    case LexicalClass(lexicalClass: LexicalClass)    
+protocol Token {
 }
 
-enum Context {
-    case Address(components: [String: String])
+protocol TokenizerPlugin {
+    func process(text: String) -> [Token]
+}
+
+//MARK: - Linguistic Tagger Plugins
+
+enum LinguisticTag: Token {
+    case Lemma(lemma: String)
+    case LexicalClass(`class`: String)
+    case LexicalType(type: String)
+}
+
+//MARK: - Data Detector plugin
+
+enum DetectedData: Token {
     case PhoneNumber(phoneNumber: String)
     case Link(URL: NSURL)
-    case Transit(airline: String?, flight: String?)
+    case Date(date: NSDate, duration: NSTimeInterval?, timeZone: NSTimeZone?)
+    case Address(components: [String: String])
+    case Transit(airline: String?, flight: String)
 }
 
-public enum LexicalClass {
-    case Verb
-    case Adjective
+private extension DetectedData {
+    init?(result: NSTextCheckingResult) {
+        let type = result.resultType
+        
+        if type.contains(.Link) {
+            self = .Link(URL: result.URL!)
+        } else if type.contains(.PhoneNumber) {
+            self = .PhoneNumber(phoneNumber: result.phoneNumber!)
+        } else if type.contains(.Address) {
+            self = .Address(components: result.addressComponents!)
+        } else if type == . {
+        } else {
+            return nil
+        }
+    }
+}
+
+
+public struct DataDetectorPlugin: TokenizerPlugin {
+    struct Kinds: OptionSetType {
+        let rawValue: Int
+        
+        init(rawValue: Int) { self.rawValue = rawValue }
+        
+        static let PhoneNumber = Kinds(rawValue: 1)
+        static let Link = Kinds(rawValue: 2)
+        static let Date = Kinds(rawValue: 4)
+        static let Address = Kinds(rawValue: 8)
+        static let Transit = Kinds(rawValue: 16)
+        
+        var dataDetectorTypes: NSTextCheckingType {
+            return .Link
+        }
+    }
     
-    public init?(_ value: String) {
-        return nil
+    let kinds: Kinds
+    private let detector: NSDataDetector
+    
+    init(kinds: Kinds) {
+        self.kinds = kinds
+        self.detector = try! NSDataDetector(types: kinds.dataDetectorTypes.rawValue)
+    }
+    
+    func process(text: String) -> [Token] {
+        
+    }
+}
+
+//MARK: - Tokenizer
+
+struct Tokenizer {
+    struct TokenResult {
+        let string: String
+        let clusters: [TokenCluster]
+    }
+    
+    struct TokenCluster {
+        let range: Range<String.Index>
+        let tokens: [Token]
+    }
+    
+    init(plugins: [TokenizerPlugin]) {
+        
+    }
+    
+    func process(string: String) -> Result {
+        
     }
 }
